ffe12f22bc718897b564d1e8c1704cd42c261a4b
Aman Sharma <jnuamansharma@gmail.com>
2023-02-27 19:04:38 +0530

    till all tables model created

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import User
+from .models import broodstock
+from .models import cliental
+from .models import cost
+from .models import hatchery
+from .models import production
+from .models import species
+from .models import sale
+
+
+
+
+
 
 # Register your models here.
 
@@ -7,3 +19,32 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id','accreditation_id','hatchery_id','accreditation_agency','term','year_of_accreditation')
 
+@admin.register(broodstock)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','hatchery_id','broodstock_type','name_broodstock','mgt_procedure_broodstock','no_ponds_broodstock','no_spawn_produce','species_id','no_fry_producing','no_nursery_ponds')
+
+@admin.register(cliental)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','hatchery_id','clients','years','clients_id')
+
+@admin.register(cost)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','hatchery_id','cost_spawn','cost_fry','cost_fingerling','cost_yearling')
+
+
+@admin.register(hatchery)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','hatchery_id','hatchery_name','hatchery_name','hatchery_type','hatchery_category','ownership','owner_name','hatchery_address','species_breeding','city')
+
+
+@admin.register(production)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','production_id','production_seed_type','broodstock_id','no_ponds_mgt','tech_seed_production','seed_production_year','mgt_procedures')
+
+@admin.register(species)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','species_id','hatchery_id','species_name')
+
+@admin.register(sale)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','hatchery_id','spawn_sale_year','fry_sale_year','fingerling_sale_year','yearling_sale_year','species_id')
--- enroll/models.py
+++ enroll/models.py
@@ -9,3 +9,73 @@ class User(models.Model):
     term = models.CharField(max_length=70)
     year_of_accreditation = models.IntegerField()
 
+
+class broodstock(models.Model):
+    hatchery_id = models.IntegerField()
+    broodstock_type = models.CharField(max_length=70)
+    name_broodstock = models.CharField(max_length=70)
+    mgt_procedure_broodstock = models.IntegerField()
+    no_ponds_broodstock = models.CharField(max_length=70)
+    no_spawn_produce = models.CharField(max_length=70)
+    species_id = models.IntegerField()
+    no_fry_producing = models.IntegerField()
+    no_nursery_ponds = models.CharField(max_length=70)
+
+class cliental(models.Model):
+    hatchery_id = models.IntegerField()
+    clients = models.CharField(max_length=70)
+    years = models.IntegerField()
+    clients_id = models.IntegerField()
+
+class cost(models.Model):
+    hatchery_id = models.IntegerField()
+    cost_spawn = models.CharField(max_length=70)
+    cost_fry = models.CharField(max_length=70)
+    cost_fingerling = models.IntegerField()
+    cost_yearling = models.IntegerField()
+
+class hatchery(models.Model):
+    hatchery_id = models.IntegerField()
+    hatchery_name = models.CharField(max_length=70)
+    hatchery_type = models.CharField(max_length=70)
+    hatchery_category = models.IntegerField()
+    ownership = models.CharField(max_length=70)
+    owner_name = models.CharField(max_length=70)
+    hatchery_address = models.CharField(max_length=70)
+    species_breeding = models.CharField(max_length=70)
+    city = models.CharField(max_length=70)
+
+class production(models.Model):
+    production_id = models.IntegerField()
+    production_seed_type = models.CharField(max_length=70)
+    broodstock_id = models.IntegerField()
+    no_ponds_mgt = models.IntegerField()
+    tech_seed_production = models.CharField(max_length=70)
+    seed_production_year = models.CharField(max_length=70)
+    mgt_procedures = models.IntegerField()
+
+class species(models.Model):
+    species_id = models.IntegerField()
+    hatchery_id = models.IntegerField()
+    species_name = models.CharField(max_length=70)
+
+
+class sale(models.Model):
+    hatchery_id = models.IntegerField()
+    spawn_sale_year = models.CharField(max_length=70)
+    fry_sale_year = models.CharField(max_length=70)
+    fingerling_sale_year = models.IntegerField()
+    yearling_sale_year = models.IntegerField()
+    species_id = models.IntegerField()
+
+
+
+
+
+
+
+
+
+
+
+
